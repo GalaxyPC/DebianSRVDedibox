fc3e02df2411d6096f2e796acdfeb1bcf353f198
Benjamin <benjic78@hotmail.fr>
2015-02-16 08:53:11 +0100

    maj

--- setup.sh
+++ setup.sh
@@ -3,8 +3,12 @@
 # Simple Firewall script.
 #
 # Author: Benjamin.COLLEAU@GALAXYPC.fr
-wget --no-check-certificate https://github.com/GalaxyPC/DebianSRVDedibox/blob/master/firewall.sh
+wget --no-check-certificate https://github.com/GalaxyPC/DebianSRVDedibox/raw/master/firewall.sh
 cp firewall.sh firewall
+echo "- copie du script en cours..."
 rm -f firewall.sh
 chmod u+x firewall
+echo "- modification des droits d'execution..."
 cp firewall /etc/init.d/firewall
+echo "Copie vers /etc/init.d/firewall"
+exit 0
