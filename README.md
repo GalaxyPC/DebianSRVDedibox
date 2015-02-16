66954fb069fd72ee8f2a8e1000c6cb24c2fcf809
Benjamin <benjic78@hotmail.fr>
2015-02-16 09:21:00 +0100

    Version 1

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # DebianSRVH-berg-
-Debian script serveur hébergé
+Script Linux Debian pour serveur hébergé
+> Firewall.sh & setup.sh
+Ce script me permet rapidement importer mes regles iptables.
+
+INSTALLATION:
+# wget --no-check-certificate https://raw.githubusercontent.com/GalaxyPC/DebianSRVDedibox/664a01703c062cca65daa7a65d4be0a7050609e9/setup.sh
+# chmod u+x setup.sh
+# ./setup.sh
+
+Vous pouvez modifier ensuite les regles dans /etc/init.d/firewall avec l'editeur texte que vous preferez.
+
+Fonctionnement du service Firewall:
+# service firewall start|status|stop
+
+Desinstallation se fait en supprimant le script:
+# service firewall stop
+# rm -f /etc/init.d/firewall
+# update-rc.d -f firewall remove
--- setup.sh
+++ setup.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 #
-# Simple Firewall script.
+# Setup for my Firewall script.
 #
 # Author: Benjamin.COLLEAU@GALAXYPC.fr
-wget --no-check-certificate https://github.com/GalaxyPC/DebianSRVDedibox/raw/master/firewall.sh
+wget --no-check-certificate https://raw.githubusercontent.com/GalaxyPC/DebianSRVDedibox/664a01703c062cca65daa7a65d4be0a7050609e9/firewall.sh
 cp firewall.sh firewall
 echo "- copie du script en cours..."
 rm -f firewall.sh
 chmod u+x firewall
 echo "- modification des droits d'execution..."
 cp firewall /etc/init.d/firewall
-echo "Copie vers /etc/init.d/firewall"
+echo "- Copie vers /etc/init.d/firewall"
 rm -f setup.sh firewall
-echo "- Demarrage du FireWall.."
+echo "- Demarrage du FireWall..."
+update-rc.d firewall defaults
 service firewall start
+echo "- STATUS du FireWall:"
+service firewall status
 exit 0
